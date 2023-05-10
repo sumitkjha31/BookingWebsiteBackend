18c86ffa5c71701e96cc2a80ea4bfc60cb404f93
sumitkv31 <sumitkv31@gmail.com>
2023-05-10 10:01:58 +0530

    cors

--- index.js
+++ index.js
@@ -26,20 +26,43 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 //   cors({
 //     credentials: true,
 //     origin:
-//       "https://645b1055c3f38169ada4c61b--symphonious-crumble-031060.netlify.app",
+//       "*",
 //   })
 // );
-// app.use(cors());
+app.use(cors());
 
-app.use(
-  cors({
-    origin:
-      "https://645b1055c3f38169ada4c61b--symphonious-crumble-031060.netlify.app",
-    headers: ["Content-Type"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin:
+//       "https://645b1055c3f38169ada4c61b--symphonious-crumble-031060.netlify.app",
+//     headers: ["Content-Type"],
+//     credentials: true,
+//   })
+// );
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
 
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 mongoose.connect(process.env.MONGO_URL);
 
 function getUserDataFromReq(req) {
