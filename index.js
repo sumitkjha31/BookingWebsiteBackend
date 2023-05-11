59fa61ccbb3bb74d156b1440479e1e671a81a0f4
sumitkv31 <sumitkv31@gmail.com>
2023-05-11 11:29:57 +0530

    m

--- index.js
+++ index.js
@@ -100,8 +100,8 @@ app.post("/login", async (req, res) => {
           if (err) throw err;
           console.log("token /login", token);
           res
-            .cookie("token", token, { httpOnly: true, secure: true })
-            .json(userDoc);
+            .cookie("token", token, { httpOnly: false, secure: true })
+            .json({ userDoc, token });
         }
       );
     } else {
