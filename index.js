77c5dfa4c795c8813a3cebfafd15dbef90d2c1b4
sumitkv31 <sumitkv31@gmail.com>
2023-06-19 13:49:54 +0530

    m

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const history = require('express-history-api-fallback');
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User.js");
